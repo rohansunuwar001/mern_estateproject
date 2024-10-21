7ec96b475ecda7c9e0c9ebfca42a754ab7296cd1
rohansunuwar07 <rohansunuwar018@outlook.com>
2024-10-21 11:35:30 +0545

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
  const app = express();
 
+ mongoose.connect(process.env.MONGO).then(() => {
+   console.log("Connected to MongoDB!");
+ }
+ ).catch((err) => {
+   console.log(err)
+ }
+ );
+
+ 
  const port = 3000;
 
  app.listen(port,() => {
