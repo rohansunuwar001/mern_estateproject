6aad3395cbfbe17d07d8e317c8746b5c6098a23b
rohansunuwar07 <rohansunuwar018@outlook.com>
2024-10-21 10:59:39 +0545

    create header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,12 @@ import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return<BrowserRouter>
-
+<Header />
   <Routes>
     <Route path="/" element={<Outlet />}>
     <Route index element={<Home />}></Route>
@@ -18,7 +20,7 @@ const App = () => {
     <Route path="about" element={<About />}></Route>
     </Route>
   </Routes>
-
+<Footer />
   </BrowserRouter>;
 };
 
